d8ca27fb31b7343950f259e1d3f06f113be5e920
samue-cpu <samuel.barzola.27@unsch.edu.pe>
2024-09-11 21:52:16 -0500

    Add files via upload

---
+++ laboratorio1.py
@@ -0,0 +1,118 @@
+#lungitud de string ejercicio 1
+nombre = "samuel"
+print(len(nombre))
+
+#comparar la longitud ejercicio 2
+nombre = "samuel"
+apellido = "barzola"
+print(len(nombre))
+print(len(apellido))
+comparacion = nombre == apellido
+print (comparacion)
+comparacion = nombre != apellido
+print(comparacion)
+
+
+#operaciones ejercicio 3
+
+#sume
+num_one = 5
+num_two= 4
+total = num_one + num_two 
+print(total)
+
+#resta
+num_one = 5
+num_two= 4
+diff = num_one - num_two
+print(diff)
+
+#▪	Multiplicacion
+num_one = 5
+num_two= 4
+producto = num_one * num_two
+print(producto)
+
+# division 
+num_one = 5
+num_two= 4
+division = num_one/num_two
+print(division)
+ 
+ # realizando devision y dar como resultado 4 o num_two
+num_one=5
+num_three=20
+resto = num_three/num_one
+print(resto)
+ 
+ # potencia
+num_one = 5
+num_two= 4
+exp= num_one**num_two
+print(exp)
+
+# división del piso 
+num_one= 5
+num_two= 4
+floor_division = num_one//num_two
+print(floor_division)
+
+# la area de circulo. sabiendo el radio que es 30
+r=30
+pi = 3.14
+area_of_circle = pi* r**2
+print(area_of_circle)
+
+#calcular circunferencia 
+r = 30
+circum_of_circle = 2*r
+print(circum_of_circle)
+
+# ▪	Tome el radio como entrada del usuario 
+radio = int(input("el radio es:"))
+area= 3.14*radio**2
+print("el are del circulo es:",area)
+
+# funcion de intrada incorporada 
+
+nombre = input("cual es tu nombre:")
+apellido = input("dime su apellido:")
+ciudad = input("en donde vives:")
+edad = input("cual es su edad:")
+
+print(" su nombre es:", nombre)
+print("su apellido es:", apellido)
+print("vive en:", ciudad)
+print("su edad es:", edad)
+
+
+
+nombre = 'Pedro'
+
+if nombre == 'Fred':
+
+    print('Hola Fred')
+
+elif nombre == 'Juan':
+
+    print('Hola Juan')
+
+elif nombre == 'Pedro':
+
+    print('Hola Pedro')
+
+elif nombre == 'Arnold':
+
+    print('Hola Arnold')
+
+else:
+
+    
+ 
+ 
+
+
+
+
+
+
